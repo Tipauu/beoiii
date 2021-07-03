a6edd0d8344b6b2c42ed607a5eddb723146bd67c
Tipauu <86764809+Tipauu@users.noreply.github.com>
2021-07-03 15:00:21 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Hem từ chối được đâu, thương bé..  ";
+    var text = " Bé thương anh lắm, iu bae của em nữaaa..:333  ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
